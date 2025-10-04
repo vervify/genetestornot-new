5c591c834fb611f496a694b657f68e3b133fd50a
vervify <michael@breakneckturtles.com>
2025-10-04 13:26:39 -0400

    fix firefox js

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,5 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 
 # jQuery support for existing functionality
-pin 'jquery', to: 'https://cdn.jsdelivr.net/npm/jquery@3.7.0/dist/jquery.js'
-pin 'jquery-ui', to: 'https://cdn.jsdelivr.net/npm/jquery-ui@1.14.1/dist/jquery-ui.min.js', preload: true
+pin 'jquery', to: 'https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js'
+pin 'jquery-ui', to: 'https://cdn.jsdelivr.net/npm/jquery-ui@1.14.1/dist/jquery-ui.min.js'
